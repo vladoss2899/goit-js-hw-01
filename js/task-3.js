b08290fd4bdb00a5537b4c3e21531ee83223cc18
vladoss2899 <vladoss2899@gmail.com>
2023-10-25 21:41:20 +0200

    a

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function getElementWidth(content, padding, border) {
   const totalWidth =
     Number.parseFloat(content) +
@@ -5,6 +7,6 @@ function getElementWidth(content, padding, border) {
   return totalWidth;
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(getElementWidth('50px', '8px', '4px'));
+console.log(getElementWidth('60px', '12px', '8.5px'));
+console.log(getElementWidth('200px', '0px', '0px'));
